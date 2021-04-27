ed634e6c4bd481e0a0562dfa9af86918c70b4399
Guilherme Salustiano <guissalustiano@gmail.com>
2021-04-27 07:12:41 -0300

    refa: refactory train

--- fracture_detection/train.py
+++ fracture_detection/train.py
@@ -8,39 +8,48 @@ import fracture_detection.datasets.mura  # NOQA
 
 AUTOTUNE = tf.data.AUTOTUNE
 
+_data_augmentation = tf.keras.Sequential([
+    tf.keras.layers.experimental.preprocessing.RandomFlip("horizontal"),
+    tf.keras.layers.experimental.preprocessing.RandomRotation(0.2),
+])
+
+
+def _freeze_model(
+    model: tf.keras.Model,
+    freeze: Union[bool, int, float] = False,
+):
+    # Obs:
+    # When you set layer.trainable = False, the BatchNormalization layer will
+    # run in inference mode, and will not update its mean and variance statistics
+    # https://www.tensorflow.org/tutorials/images/transfer_learning#important_note_about_batchnormalization_layers
+
+    if isinstance(freeze, int):
+        freeze_len = freeze
+    elif isinstance(freeze, float):
+        freeze_len = int(freeze*len(model.layers))
+    else:  # isinstance(freeze, bool):
+        if freeze:
+            freeze_len = len(model.layers)
+        else:
+            freeze_len = 0
+
+    if freeze_len != len(model.layers):
+        model.trainable = True
+
+        for layer in model.layers[:freeze_len]:
+            layer.trainable = False
+
 
 def generate_model(
     base_model: tf.keras.Model,
     img_shape: Tuple[Optional[int], Optional[int], Optional[int]],
     freeze: Union[bool, int, float] = False,
     preprocess_input: Optional[Callable] = None,
-    data_augmentation: bool = True,
+    use_data_augmentation: bool = True,
 ):
-    if isinstance(freeze, int):
-        freeze_len = freeze
-    elif isinstance(freeze, float):
-        freeze_len = int(freeze*len(base_model.layers))
-    else:  # isinstance(freeze, bool):
-        if freeze:
-            freeze_len = len(base_model.layers)
-        else:
-            freeze_len = 0
-
-    data_augmentation_layers = tf.keras.Sequential(
-        [
-            tf.keras.layers.experimental.preprocessing.RandomFlip("horizontal"),
-            tf.keras.layers.experimental.preprocessing.RandomRotation(0.2),
-        ]
-    )
-
-    # Obs:
-    # When you set layer.trainable = False, the BatchNormalization layer will
-    # run in inference mode, and will not update its mean and variance statistics
-    # https://www.tensorflow.org/tutorials/images/transfer_learning#important_note_about_batchnormalization_layers
-
     inputs = tf.keras.layers.Input(shape=img_shape)
-    if data_augmentation:
-        x = data_augmentation_layers(inputs)
+    if use_data_augmentation:
+        x = _data_augmentation(inputs)
     if preprocess_input is not None:
         x = preprocess_input(inputs)
     x = base_model(x, training=False)
@@ -50,12 +59,7 @@ def generate_model(
 
     model = tf.keras.Model(inputs, outputs)
 
-    if freeze_len != len(base_model.layers):
-        model.trainable = True
-        base_model.trainable = True
-
-        for layer in base_model.layers[:freeze_len]:
-            layer.trainable = False
+    _freeze_model(base_model, freeze)
 
     base_learning_rate = 0.0001
     model.compile(
@@ -83,6 +87,30 @@ def _prepare_ds(
     )
 
 
+def _train_calbacks(
+    checkpoint_path: Path,
+    save_freq: int = 5,
+):
+    checkpoint_path.mkdir(parents=True, exist_ok=True)
+
+    save_model_period_callback = tf.keras.callbacks.ModelCheckpoint(
+        filepath=checkpoint_path / "{epoch:04d}.h5",
+        verbose=1,
+        period=save_freq,
+    )
+    save_model_callback = tf.keras.callbacks.ModelCheckpoint(
+        filepath=checkpoint_path / "final.h5",
+    )
+    save_log_calback = tf.keras.callbacks.CSVLogger(
+        filename=checkpoint_path / "train.log"
+    )
+    early_stop_calback = tf.keras.callbacks.EarlyStopping(
+        monitor='loss', min_delta=0.005, patience=3
+    )
+
+    return [save_model_period_callback, save_model_callback, save_log_calback, early_stop_calback]
+
+
 def train(
     model: tf.keras.Model,
     img_shape: Tuple[Optional[int], Optional[int], Optional[int]],
@@ -101,26 +129,11 @@ def train(
     ds_train = _prepare_ds(ds_train, img_shape=img_shape, batch_size=batch_size)
     ds_valid = _prepare_ds(ds_valid, img_shape=img_shape, batch_size=batch_size)
 
-    cpp_callback = tf.keras.callbacks.ModelCheckpoint(
-        filepath=checkpoint_path / "{epoch:04d}.h5",
-        verbose=1,
-        period=save_freq,
-    )
-    cpf_callback = tf.keras.callbacks.ModelCheckpoint(
-        filepath=checkpoint_path / "final.h5",
-        period=save_freq,
-    )
-    cl_calback = tf.keras.callbacks.CSVLogger(
-        filename=checkpoint_path / "train.log"
-    )
-    es_calback = tf.keras.callbacks.EarlyStopping(
-        monitor='loss', min_delta=0.005, patience=3
-    )
-    checkpoint_path.mkdir(parents=True, exist_ok=True)
+    callbacks = _train_calbacks(checkpoint_path, save_freq)
 
     return model.fit(
         ds_train,
         epochs=epochs,
         validation_data=ds_valid,
-        callbacks=[cpp_callback, cpf_callback, cl_calback, es_calback],
+        callbacks=callbacks,
     )
